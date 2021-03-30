b0104086829a1b64adfc825297f58822fc76573c
rushikadivar <rushikadivar2701@gmail.com>
2021-03-30 19:59:24 +0530

    question selection
    user select the options and the score will be generated

--- quiz/src/index.js
+++ quiz/src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./assets/style.css"
+import  quizService from './quizService';
+import QuestionBox from './components/QuestionBox';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Quiz extends Component {
+    state = {
+        questionBank: [],
+        score: 0,
+        attempted: 0
+    };
+    getQuestions = () => {
+        quizService().then(question => {
+            this.setState({
+                questionBank: question
+            });
+        });
+    };
+    componentDidMount() {
+        this.getQuestions();
+    };
+    computeAnswer = (answer, correctAnswer) => {
+        if (answer === correctAnswer) {
+            this.setState({
+                score: this.state.score + 1
+            });
+        }        
+        this.setState({
+            attempted : this.state.attempted < 5 ? this.state.attempted + 1 : 5
+        });
+    };
+    render() {
+        return (
+            <div className="container">
+                <div className="title">
+                    Quiz
+                </div>
+                {this.state.questionBank.length > 0 && 
+                    this.state.attempted < 5 &&
+                    this.state.questionBank.map(
+                        ({question, answers, correct, questionId}) => 
+                            <QuestionBox
+                                question={question}
+                                options={answers}
+                                key={questionId}
+                                selected={answer => {this.computeAnswer(answer, correct)}}
+                            />
+                        
+                    )}
+                    {this.state.attempted === 5 ? <h2>{this.state.score}</h2> : null }
+            </div>
+        );
+    }
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Quiz />, document.getElementById('root'));
